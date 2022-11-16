6ba2c73e696fc1828fbbeca4845e0f2fa61f682e
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-11-16 12:00:47 -0300

    Using POST with associations (belongs_to)

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,16 +1,16 @@
 class Contact < ApplicationRecord
-    belongs_to :kind
+    belongs_to :kind, optional: true
 
-    def author 
-        "Thiago"
-    end
+    #def author 
+    #   "Thiago"
+    #end
 
-    def kind_description
-        self.kind.description
-    end    
-    def as_json(options={})
-            super(methods: [:author], 
-              root: true, 
-              include: { kind: { only: :description }})
+    #def kind_description
+    #   self.kind.description
+    #end    
+    #def as_json(options={})
+    #        super(methods: [:author], 
+    #          root: true, 
+    #          include: { kind: { only: :description }})
     end
 end
