d173aacb0287617e9a1e97f4061139ceebf12761
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-11-16 12:59:48 -0300

    I18n for data (as_json)

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,17 +2,11 @@ class Contact < ApplicationRecord
     belongs_to :kind #optional: true
     has_many :phones
 
-    
+    def as_json(options={})
+    h = super(options)
+    h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    end
 
-    def to_br
-   {
-      name: self.name,
-      email: self.email,
-      birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
-     }
-   end
-    
-    
     #def author 
     #   "Thiago"
     #end
