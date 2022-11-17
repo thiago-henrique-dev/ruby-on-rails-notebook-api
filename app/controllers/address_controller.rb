dc196f98799690447eef9d624d0309fef428c86a
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-11-17 11:12:12 -0300

    CRUD AMS with has_one (Address)

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -1,14 +1,41 @@
 class AddressController < ApplicationController
-    before_action :set_address
+    before_action :set_contact
+
+    def destroy
+        @contact.address.destroy
+    end
     
+    
+    def create
+        @contact.address = Address.new(address_params)
+        if @contact.save
+            render json: @contact.address, status: :created, location: contact_address_url(@contact)
+        else
+            render json: @contact.errors, status: :unprocessable_entity
+    end
+
+    def update
+        if @contact.address.update(address_params)
+            render json: @contact.address
+        else
+            render json: @contact.errors, status: :unprocessable_entity
+        end
+    end
+
     def show
-        render json: @phones
+        render json: @contact.address
     end    
     
     private
         # Use callbacks to share common setup or constraints between actions.
-        def set_address
+        def set_contact
           if params[:contact_id]
-            @kind = Contact.find(params[:contact_id]).address
+            @contact = Contact.find(params[:contact_id])
         end
+
+        def adress_params
+            ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+
+        end
+        
     end
--- app/serializers/address_serializer.rb
+++ app/serializers/address_serializer.rb
@@ -1,3 +1,7 @@
 class AddressSerializer < ActiveModel::Serializer
     attributes :id, :street, :city
+
+    belongs_to :contact do
+        link(:related) { contact_kind_url(object.id) }
+    end
 end
