856bd9d7997cad3d3ed8d81f5c13eb09ed4cf5a4
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-11-17 08:56:51 -0300

    Associations

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,10 +1,14 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
+  belongs_to :kind #PERTENCE A UM TIPO
+  has_many :phones #TEM MUITOS TELEFONES
+  has_one :address #POSSUI UM ENDEREÇO
+
   def attributes(*args)
   h = super(*args)
   # pt-BR ---> h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
-  h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?)
+  h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
   h
   end
 end
